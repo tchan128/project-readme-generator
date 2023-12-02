b5bde9b05dd159849b6ff09a262d58da6bf1ad6a
Tiff Chan <tiffchan@Tiffanys-MacBook-Air.local>
2023-12-02 01:44:12 -0500

    Added packages and questions

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the name of your project?",
+    "Please describe your project (what is it, your motivation, why it's build, what problem it solves). ",
+    "Please provide installation instructions to your project.",
+    "How do you use the project? Think about what outputs are expected, how to use it, and what we can use the project for.",
+    "Add any contributions/credits when creating this project.",
+    "What testing was done for the project, and how should users test the project?",
+    "Please choose a license for this project.",
+    "What is your github username?",
+    "What is your email address?",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
